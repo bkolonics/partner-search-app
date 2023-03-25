df44b4b450152cc5db72f482e5d11141066763d4
Bence KOLONICS <placeholder@minet.net>
2023-03-25 17:26:24 +0100

    Linting

--- mvp.py
+++ mvp.py
@@ -6,26 +6,27 @@ def excel_to_dataframe(file: str, **kwargs: str) -> pd.DataFrame:
     """Converts excel file to dataframe"""
     return pd.read_excel(file, **kwargs)
 
-def plot_dataframe(df: pd.DataFrame, **kwargs: str) -> None:
+def plot_dataframe(df_to_plot: pd.DataFrame, **kwargs: str) -> None:
     """Plots dataframe"""
-    fig = df.plot(kind='bar', **kwargs).get_figure()
+    fig = df_to_plot.plot(kind='bar', **kwargs).get_figure()
     fig.savefig('EC Contribution per Project.pdf', bbox_inches='tight')
 
-def statistic(Cost: pd.DataFrame ) -> None:
+def statistic(cost_col: pd.DataFrame ) -> None:
     """function that print statistics about dataframe"""
-    print(Cost.describe())
-    print("var     ", np.var(Cost))
-    print("skew    ", Cost.skew())
-    _, bin_edges = np.histogram(Cost, bins=18)
+    print(cost_col.describe())
+    print("var     ", np.var(cost_col))
+    print("skew    ", cost_col.skew())
+    _, bin_edges = np.histogram(cost_col, bins=18)
     (_, ax) = plt.subplots()
-    ax.hist(Cost, bin_edges, cumulative = False)
-    ax.set_xlabel('Cost')
+    ax.hist(cost_col, bin_edges, cumulative = False)
+    ax.set_xlabel('cost_col')
     ax.set_ylabel('Frequency')
     plt.show()
 if __name__ == '__main__':
 
     print(excel_to_dataframe('assets/participants.xlsx', sheet_name='Sheet1'))
-    plot_dataframe(excel_to_dataframe('assets/participants.xlsx', sheet_name='Sheet1'),x="name", y="ecContribution" , figsize=(100, 30), fontsize=1, title="EC Contribution per Project")
-    print(type(excel_to_dataframe('assets/projects.xlsx', sheet_name='Sheet1')["totalCost"]))
-    print(statistic(excel_to_dataframe('assets/projects.xlsx', sheet_name='Sheet1')["totalCost"]))
+    plot_dataframe(excel_to_dataframe('assets/participants.xlsx', sheet_name='Sheet1'),x="name",
+                    y="ecContribution" , figsize=(100, 30), fontsize=1, title="EC Contribution per Project")
+    print(type(excel_to_dataframe('assets/projects.xlsx', sheet_name='Sheet1')["totalcost_col"]))
+    print(statistic(excel_to_dataframe('assets/projects.xlsx', sheet_name='Sheet1')["totalcost_col"]))
     
