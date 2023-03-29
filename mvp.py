ae5ea1ea80e21e3e7f4de735aaf254ea50471ed2
Bence KOLONICS <placeholder@minet.net>
2023-03-29 20:35:18 +0200

    FR 1.6, 1.7, 1.8

--- mvp.py
+++ mvp.py
@@ -51,6 +51,31 @@ def validate_country_acronym(aconym: str) -> str:
 
     return aconym
 
+def received_grants_per_partner_for_country(country: str, save: bool = False) -> pd.DataFrame:
+    """
+    function that returns a dataframe with the received grants per partner for a country and
+    saves it to a excel file if save is True
+    """
+    df_of_participants = excel_to_dataframe('assets/participants.xlsx',
+                                            sheet_name='Sheet1')
+
+
+    df_of_participants = df_of_participants[df_of_participants["country"] == country]
+    df_of_participants = df_of_participants[['shortName', 'name', 'activityType', 'organizationURL', 'ecContribution']]
+    count_project = df_of_participants.groupby('shortName').size().reset_index(name='count_project')
+    sum_ecContribution = df_of_participants.groupby('shortName')['ecContribution'].sum().reset_index(name='sum_ecContribution')
+    df_of_participants = pd.merge(df_of_participants, count_project, on='shortName')
+    df_of_participants = pd.merge(df_of_participants, sum_ecContribution, on='shortName')
+    df_of_participants = df_of_participants.drop_duplicates(subset='name', keep="first")
+    df_of_participants = df_of_participants.sort_values(by=['sum_ecContribution'], ascending=False)
+
+
+    if save:
+        df_of_participants.to_excel(f"output/received_grants_per_partner_for_country_{country}.xlsx")
+    
+    return df_of_participants
+
+
 
 if __name__ == '__main__':
 
@@ -60,3 +85,4 @@ if __name__ == '__main__':
                                        sheet_name='Sheet1')["totalCost"]))
 
     selected_country = validate_country_acronym(input("Enter country acronym: "))
+    print(received_grants_per_partner_for_country(selected_country , save=True))
